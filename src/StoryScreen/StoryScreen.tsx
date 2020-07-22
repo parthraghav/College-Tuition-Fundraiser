4edeff0c5b6af8eb5343f638e7adeed1bca7ca6d
Parth Raghav <iniento12@gmail.com>
2020-07-22 15:43:03 +0530

    Added StoryScreen UI

---
+++ src/StoryScreen/StoryScreen.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+export default function StoryScreen(props: any) {
+  return (
+    <div
+      style={{
+        height: "fit-content",
+        background: "#f0eef1",
+        padding: "20px",
+      }}
+    >
+      <div style={{ margin: "auto", maxWidth: 600 }}>
+        <h1 style={{ margin: 0 }}>Born different.</h1>
+        <h2 style={{ margin: 0 }}>To make a difference.</h2>
+
+        <p>
+          Today is my birthday and I feel a sense of reckless abandon spawn in
+          my chest. I came to Reed College knowing more about computers than
+          myself and left having better understood both. In spring, when I was
+          still on campus, I came out as a proud gay man to my family and
+          friends. My sexuality was for a long time a well-kept secret from
+          everyone in India just because of how conservative everyone was and
+          continues to be. That is until I came to Reed. I met gay men and
+          women, non-binary seniors taking Computer Science courses. I saw
+          myself free in their freedom of expression, and every second spent
+          with them gave me an ounce of courage to come out in the open as well.
+        </p>
+
+        <p>
+          When I came out, I felt refreshed, reset of all guilts, and proud to
+          accept myself and allow others to do the same. However, the magic of
+          the moment soon dissolved into the grim reality. You see I wanted to
+          come out as publicly as I could because I couldn’t risk my family
+          shutting me down yet again. So, I chose Instagram and my coming-out
+          post on Instagram reached nearly around 11,000 impressions. The word
+          spread like wildfire. People whom I didn’t know knew me now knew, and
+          everyone wanted to talk me out of this or share a piece of their mind.
+          I dealt with it all alone, but the worst was to come. My dad had
+          borrowed money from local financiers to get me through college, a
+          common practice in India. However, all three of them rescinded their
+          financial support. Others didn’t want to be connected with my dad. So,
+          they cut him off. My dad paid for my education up until I came out,
+          and then he just couldn’t. As I saw my dream of education crumble, I
+          went to Student Office, Business Office, Financial Aid Office, ISS,
+          faculty members, my tears were met with their inability to do
+          anything, strict policies to ensure fairness? — and just like that, in
+          days, my aspiration to get a college degree was choked out. I withdrew
+          from the college, after having given my last bit of energy in
+          convincing that I did not deserve to have my education stopped like
+          that just because I came out as gay.
+        </p>
+
+        <p>
+          You see in some way, I was preparing for this, slowly and steadily.
+          When I was bullied a lot in high school, my dad told me "education and
+          passion is your one-way ticket to a better life." I took his words to
+          heart and seized every opportunity to learn. When I exhausted
+          resources in high school, I discovered the art of computer programming
+          and made tons of self-guided projects. My passion for scientific
+          communication and hobbyist development won me awards at Intel
+          International Science and Engineering Fair, Google Science Fair,
+          American Association of Computing Machinery, Indian National Science
+          Fair, et cetera. I learned a lot from a computer and an internet
+          connection. And yet, all of that amounted to nothing when I picked
+          each and everyone’s call as they came in shouting. My achievements and
+          hard work and struggle didn’t matter when I went to offices in the
+          Elliot, asking for financial aid. It didn’t matter when I wanted to
+          register at a local university in my hometown a week ago because I
+          didn’t have a transcript. I asked the business office for a transcript
+          but because they refused to release my transcript or any form of proof
+          of attendance since I have uncleared dues of last semester. This means
+          not only I cannot go to Reed anymore, but any university anywhere in
+          the world, even my hometown. Things I did for the last 6 years of my
+          life to get an education so that I can live as a free gay man didn’t
+          matter. None of it mattered, and now I am back to being the afraid
+          fifteen-year-old who is clueless about how he can be free.
+        </p>
+
+        <p>
+          President Audrey, I write to you because I am broken. Education is
+          important to me to escape the life of struggle my mom and dad have
+          seen. I request you to release my transcripts. I promise I will repay
+          the remaining $24,802.62 (dues for the last semester) as soon as I get
+          a job. I need transcripts to complete a degree elsewhere and I don’t
+          have the money right now to pay up my dues.{" "}
+        </p>
+
+        <blockquote>
+          oh no, must it be raining so much,
+          <br />
+          my corner eyes ocean to your touch.
+          <br />
+          i'm done being planned for.
+          <br />
+          i'm done being an image... of your shadow.
+          <br />
+          i'm the light that i emit.
+          <br />
+          i'm a soldier, i fight for my truth. <br />
+          i lose in valor if i lose to you <br />
+        </blockquote>
+      </div>
+    </div>
+  );
+}
