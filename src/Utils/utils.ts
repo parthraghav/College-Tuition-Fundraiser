d34942de7408b726a42c062ec4b1421894652686
Parth Raghav <iniento12@gmail.com>
2020-07-24 21:59:30 +0530

    Adding a viewport function

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -6,3 +6,14 @@ export enum ScrollDirection {
   Negative, // Going up
   Positive, // Going down
 }
+/**
+ * Check if an element is in viewport
+ *
+ * @param {number} [offset]
+ * @returns {boolean}
+ */
+export function isInViewport(offset = 0, el: any) {
+  if (!el) return false;
+  const top = el.getBoundingClientRect().top;
+  return top + offset >= 0 && top - offset <= window.innerHeight;
+}
