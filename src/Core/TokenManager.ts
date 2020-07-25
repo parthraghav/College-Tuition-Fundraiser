eb3c5165a312747820b7e30f2bdcf0515a1cc030
Parth Raghav <iniento12@gmail.com>
2020-07-26 01:38:40 +0530

    Integrating Stripe SDK with FundScreen

--- src/Core/TokenManager.ts
+++ src/Core/TokenManager.ts
@@ -1,3 +1,25 @@
-export const tokenHandler = (token: any) => {
+import { FirebaseApp } from "./Firebase";
+
+export const tokenHandler = (token: any, amount: number) => {
+  const transactedAmount = amount * 100;
+  const { id, email } = token;
   console.log(token);
+  processPayment(token, id, email, transactedAmount);
+};
+
+export const processPayment = async (
+  token: any,
+  id: string,
+  email: string,
+  transactedAmount: number
+) => {
+  const paymentsRef = FirebaseApp.db.collection("payments");
+  const _docId = id.slice(4, id.length);
+  await paymentsRef.doc(_docId).set({
+    id: id,
+    email: email,
+    token: token,
+    amount: transactedAmount,
+    currency: "USD",
+  });
 };
