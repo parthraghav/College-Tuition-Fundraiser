da24e7e9a92289e2b1b182cf0d43c59957dcad88
Parth Raghav <iniento12@gmail.com>
2020-07-26 18:22:56 +0530

    Adding callbacks and displayname fields

--- src/Core/TokenManager.ts
+++ src/Core/TokenManager.ts
@@ -1,25 +1,39 @@
 import { FirebaseApp } from "./Firebase";
 
-export const tokenHandler = (token: any, amount: number) => {
+export const tokenHandler = (
+  token: any,
+  amount: number,
+  name: string,
+  callbacks: any
+) => {
   const transactedAmount = amount * 100;
   const { id, email } = token;
   console.log(token);
-  processPayment(token, id, email, transactedAmount);
+  processPayment(token, id, email, transactedAmount, name, callbacks);
 };
 
 export const processPayment = async (
   token: any,
   id: string,
   email: string,
-  transactedAmount: number
+  transactedAmount: number,
+  name: string,
+  callbacks: any
 ) => {
-  const paymentsRef = FirebaseApp.db.collection("payments");
-  const _docId = id.slice(4, id.length);
-  await paymentsRef.doc(_docId).set({
-    id: id,
-    email: email,
-    token: token,
-    amount: transactedAmount,
-    currency: "USD",
-  });
+  try {
+    const paymentsRef = FirebaseApp.db.collection("payments");
+    const _docId = id.slice(4, id.length);
+    console.log(name);
+    await paymentsRef.doc(_docId).set({
+      id: id,
+      email: email,
+      name: name,
+      token: token,
+      amount: transactedAmount,
+      currency: "USD",
+    });
+    callbacks.success();
+  } catch (e) {
+    callbacks.error();
+  }
 };
