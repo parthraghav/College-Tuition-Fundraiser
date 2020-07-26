34481af0bfbeb5774b292752dcf2ef65638792d8
Parth Raghav <iniento12@gmail.com>
2020-07-26 15:17:56 +0530

    Adding StripeManager

---
+++ functions/src/Core/stripe.ts
@@ -0,0 +1,19 @@
+import * as CloudFunctions from "firebase-functions";
+
+const stripe = require("stripe")(CloudFunctions.config().stripe.testkey);
+
+interface Charge {
+  amount: number;
+  currency: string;
+  source: string;
+}
+
+// Returns a stripe charge object that stripe sends
+export const createCharge = async (
+  charge: Charge,
+  idempotencyToken: string
+) => {
+  return await stripe.charges.create(charge, {
+    idempotency_key: idempotencyToken,
+  });
+};
