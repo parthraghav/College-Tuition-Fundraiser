215895a5a5c4a43a1d91dfe411a281c5ed595145
Parth Raghav <iniento12@gmail.com>
2020-07-31 01:48:15 +0530

    Adding images, changing styles

--- src/StoryScreen/StoryScreen.tsx
+++ src/StoryScreen/StoryScreen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles.css";
 
 export default function StoryScreen(props: any) {
   return (
@@ -7,16 +8,15 @@ export default function StoryScreen(props: any) {
         position: "relative",
         height: "fit-content",
         background: "rgb(255, 253, 253)",
-        padding: "20px",
-        paddingTop: "80px",
-        paddingBottom: "80px",
       }}
     >
-      <div style={{ margin: "auto", maxWidth: 600 }}>
-        <h1 style={{ margin: 0 }}>Born different.</h1>
-        <h2 style={{ margin: 0 }}>To make a difference.</h2>
+      <div className="letter">
+        <div className="textblock">
+          <h1 style={{ margin: 0 }}>🌈 Born different.</h1>
+          <h2 style={{ margin: 0 }}>To make a difference.</h2>
+        </div>
 
-        <p>
+        <p className="textblock">
           Today is my birthday and I feel a sense of reckless abandon spawn in
           my chest. I came to Reed College knowing more about computers than
           myself and left having better understood both. In spring, when I was
@@ -29,38 +29,65 @@ export default function StoryScreen(props: any) {
           with them gave me an ounce of courage to come out in the open as well.
         </p>
 
-        <p>
-          When I came out, I felt refreshed, reset of all guilts, and proud to
-          accept myself and allow others to do the same. However, the magic of
-          the moment soon dissolved into the grim reality. You see I wanted to
-          come out as publicly as I could because I couldn’t risk my family
-          shutting me down yet again. So, I chose Instagram and my coming-out
-          post on Instagram reached nearly around 11,000 impressions. The word
-          spread like wildfire. People whom I didn’t know knew me now knew, and
-          everyone wanted to talk me out of this or share a piece of their mind.
-          I dealt with it all alone, but the worst was to come. My dad had
-          borrowed money from local financiers to get me through college, a
-          common practice in India. However, all three of them rescinded their
-          financial support. Others didn’t want to be connected with my dad. So,
-          they cut him off. My dad paid for my education up until I came out,
-          and then he just couldn’t. As I saw my dream of education crumble, I
-          went to Student Office, Business Office, Financial Aid Office, ISS,
-          faculty members, my tears were met with their inability to do
-          anything, strict policies to ensure fairness? — and just like that, in
-          days, my aspiration to get a college degree was choked out. I withdrew
-          from the college, after having given my last bit of energy in
-          convincing that I did not deserve to have my education stopped like
-          that just because I came out as gay.
+        <p className="textblock">
+          <a href="https://www.instagram.com/p/B9D7R64JeGz/">
+            When I came out, I felt refreshed
+          </a>
+          , reset of all guilts, and proud to accept myself and allow others to
+          do the same. However, the magic of the moment soon dissolved into the
+          grim reality. You see I wanted to come out as publicly as I could
+          because I couldn’t risk my family shutting me down yet again. So, I
+          chose Instagram and my coming-out post on Instagram reached nearly
+          around 11,000 impressions. The word spread like wildfire. People whom
+          I didn’t know knew me now knew, and everyone wanted to talk me out of
+          this or share a piece of their mind. I dealt with it all alone, but
+          the worst was to come. My dad had borrowed money from local financiers
+          to get me through college, a common practice in India. However, all
+          three of them rescinded their financial support. Others didn’t want to
+          be connected with my dad. So, they cut him off. My dad paid for my
+          education up until I came out, and then he just couldn’t. As I saw my
+          dream of education crumble, I went to Student Office, Business Office,
+          Financial Aid Office, ISS, faculty members, my tears were met with
+          their inability to do anything, robotic replies, and strict policies
+          to ensure fairness? — and just like that, in days, my aspiration to
+          get a college degree was choked out. I withdrew from the college,
+          after having given my last bit of energy in convincing that I did not
+          deserve to have my education stopped like that just because I came out
+          as gay. Being gay is not a choice, being proud of it is!
         </p>
+        <div className="imageHolder">
+          <a
+            target="_blank"
+            className="disable-highlight"
+            href="https://firebasestorage.googleapis.com/v0/b/parthraghav-com.appspot.com/o/fund%2Fstatic%2FEmailScreenshot1.png?alt=media"
+          >
+            <img src="https://firebasestorage.googleapis.com/v0/b/parthraghav-com.appspot.com/o/fund%2Fstatic%2FEmailScreenshot1.png?alt=media" />
+          </a>
+          <a
+            target="_blank"
+            className="disable-highlight"
+            href="https://firebasestorage.googleapis.com/v0/b/parthraghav-com.appspot.com/o/fund%2Fstatic%2FEmailScreenshot2.png?alt=media"
+          >
+            <img src="https://firebasestorage.googleapis.com/v0/b/parthraghav-com.appspot.com/o/fund%2Fstatic%2FEmailScreenshot2.png?alt=media" />
+          </a>
+          <a
+            target="_blank"
+            className="disable-highlight"
+            href="https://firebasestorage.googleapis.com/v0/b/parthraghav-com.appspot.com/o/fund%2Fstatic%2FEmailScreenshot3.png?alt=media"
+          >
+            <img src="https://firebasestorage.googleapis.com/v0/b/parthraghav-com.appspot.com/o/fund%2Fstatic%2FEmailScreenshot3.png?alt=media" />
+          </a>
+        </div>
 
-        <p>
+        <p className="textblock">
           You see in some way, I was preparing for this, slowly and steadily.
           When I was bullied a lot in high school, my dad told me "education and
           passion is your one-way ticket to a better life." I took his words to
           heart and seized every opportunity to learn. When I exhausted
           resources in high school, I discovered the art of computer programming
           and made tons of self-guided projects. My passion for scientific
-          communication and hobbyist development won me awards at Intel
+          communication and hobbyist development won me{" "}
+          <a href="http://www.parthraghav.com/">awards</a> at Intel
           International Science and Engineering Fair, Google Science Fair,
           American Association of Computing Machinery, Indian National Science
           Fair, et cetera. I learned a lot from a computer and an internet
@@ -71,37 +98,47 @@ export default function StoryScreen(props: any) {
           register at a local university in my hometown a week ago because I
           didn’t have a transcript. I asked the business office for a transcript
           but because they refused to release my transcript or any form of proof
-          of attendance since I have uncleared dues of last semester. This means
-          not only I cannot go to Reed anymore, but any university anywhere in
-          the world, even my hometown. Things I did for the last 6 years of my
-          life to get an education so that I can live as a free gay man didn’t
-          matter. None of it mattered, and now I am back to being the afraid
-          fifteen-year-old who is clueless about how he can be free.
+          of successfully completing first year since I have uncleared dues of
+          last semester. This means not only I cannot go to Reed anymore, but
+          any university anywhere in the world, even my hometown. Things I did
+          for the last 6 years of my life to get an education so that I can live
+          as a free gay man didn’t matter. None of it mattered, and now I am
+          back to being the afraid fifteen-year-old who is clueless about how he
+          can be free.
         </p>
 
-        <p>
-          President Audrey, I write to you because I am broken. Education is
-          important to me to escape the life of struggle my mom and dad have
-          seen. I request you to release my transcripts. I promise I will repay
-          the remaining $24,802.62 (dues for the last semester) as soon as I get
-          a job. I need transcripts to complete a degree elsewhere and I don’t
-          have the money right now to pay up my dues.{" "}
+        <p className="textblock">
+          I wrote this letter to the president, to an equally disappointing end.
+          I am making this website to share with everybody my story. Education
+          is important to me to escape the life of struggle my mom and dad have
+          seen. I request you to help me release my transcripts by paying the
+          remaining $24,802.62 (dues for the last semester). I promise to repay
+          all those who help me as soon as I can. I need transcripts to complete
+          a degree elsewhere and I don’t have the money right now to pay up my
+          dues.
         </p>
-
-        <blockquote>
-          oh no, must it be raining so much,
-          <br />
-          my corner eyes ocean to your touch.
-          <br />
-          i'm done being planned for.
-          <br />
-          i'm done being an image... of your shadow.
-          <br />
-          i'm the light that i emit.
-          <br />
-          i'm a soldier, i fight for my truth. <br />
-          i lose in valor if i lose to you <br />
-        </blockquote>
+        <p className="textblock">
+          Poetry continues to be my last form of expression. Here's an excerpt
+          from a <a href="https://www.instagram.com/p/B-zUOW6FAhD/">poem</a> I
+          wrote for the Poetry Night at Reed (cancelled due to COVID).
+        </p>
+        <div className="textblock excerpt">
+          <blockquote>
+            oh no, must it be raining so much,
+            <br />
+            my corner eyes ocean to your touch.
+            <br />
+            i'm done being planned for.
+            <br />
+            i'm done being an image... of your shadow.
+            <br />
+            i'm the light that i emit.
+            <br />
+            i'm a soldier, i fight for my truth. <br />
+            i lose in valor if i lose to you <br />
+          </blockquote>
+        </div>
+        <p className="textblock">Thank you for your time!</p>
       </div>
     </div>
   );
