f6b7b58077c05b917a292e360c6b5a0a53c41bd8
Parth Raghav <iniento12@gmail.com>
2020-07-31 17:21:15 +0530

    Added analytics

--- src/Core/Firebase/Firebase.ts
+++ src/Core/Firebase/Firebase.ts
@@ -1,12 +1,15 @@
 import app from "firebase/app";
+import "firebase/analytics";
 import "firebase/firestore"; // <- needed if using firestore
 import { FIREBASE_CONFIG } from "./Config";
 
 class Firebase {
   db: app.firestore.Firestore;
+  analytics: app.analytics.Analytics;
   constructor() {
     app.initializeApp(FIREBASE_CONFIG);
     this.db = app.firestore(); // <- needed if using firestore
+    this.analytics = app.analytics();
   }
 }
 
